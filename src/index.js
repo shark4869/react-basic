6d8c88ea7a07bd18af83dffcee5916c219bcd374
H Uyen Mlo <uyenuyenbt4869@gmail.com>
2023-03-13 15:03:45 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -4,31 +4,21 @@ import './styles/global.scss';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
-import Button from './views/Hook/Test';
+
 
 import App from './views/App';
 import reportWebVitals from './reportWebVitals';
-// const reduxStore = createStore(rootReducer,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={reduxStore}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>, 
-//   document.getElementById('root'));
-  ReactDOM.render(
+const reduxStore = createStore(rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+ReactDOM.render(
   <React.StrictMode>
-      <Button />
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>
   </React.StrictMode>, 
   document.getElementById('root'));
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+  
 
 
 reportWebVitals();
